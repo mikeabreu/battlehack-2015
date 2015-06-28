31ac102d08e3af74018a6355f4b88ab8c420fb59
Michael Abreu <dev.mikeabreu@gmail.com>
2015-06-27 19:20:16 -0500

    Added routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,23 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/charity', function () {
+    return view('charity');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
