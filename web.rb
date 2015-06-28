571531e1e02e0d9703dae100802ce8b7ca9b2840
Michael Abreu <dev.mikeabreu@gmail.com>
2015-06-28 00:54:28 -0500

    Commited

--- web.rb
+++ web.rb
@@ -82,7 +82,7 @@ class Payday < Sinatra::Base
         end
     end
 
-    get 'login' do
+    get '/login' do
         @google_login = '<meta name="google-signin-scope" content="profile email">
 <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
 <script src="https://apis.google.com/js/platform.js" async defer></script>';
